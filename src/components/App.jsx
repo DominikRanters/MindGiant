9ccf5cae8b0a1f8231f5deaf7c9b20bb99733712
DRanters <d.ranters@googlemail.com>
2020-01-20 16:04:19 +0100

    Add folder and files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import Header from './Header/Header';
+import './app.scss';
 
-const App = () => <h1>Hello world!</h1>;
+const App = () => (
+    <div className="app">
+        <Header />
+    </div>
+);
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './header.scss';
+// import lightblub from '../../images/lightblub.png';
+
+const Header = () => {
+
+    return (
+        <div className="header">
+            {/*<img src={lightblub} alt="header image" />*/}
+            <div className="headline">MindGiant</div>
+        </div>
+    );
+};
+
+export default Header;
